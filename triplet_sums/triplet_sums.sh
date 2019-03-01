8387b9b97dfeff0a8623c26ceaa0dd0ea76c4727
Alex Gegechkori <gecktor@gmail.com>
2019-03-01 12:42:58 +0600

    Modified testing script.

--- triplet_sums/triplet_sums.sh
+++ triplet_sums/triplet_sums.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-awk -f triplet_sums.awk triplet_sums_test_input.txt
+awk -f triplet_sums.awk triplet_sums_test_input.txt | tee solution.txt
+
+awk -f triplet_sums_brute_force.awk triplet_sums_test_input.txt | tee solution_brute_force.txt
+
+difference=$(diff solution.txt solution_brute_force.txt)
+
+if [ "${difference}" ]; then
+    echo "Oh, no!"
+fi
